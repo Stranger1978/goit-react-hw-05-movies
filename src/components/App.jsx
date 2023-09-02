a31fe36e47036b5acc9a4c5d3ce5a7394ace1af2
Pavlo Yeromenko <108014692+Stranger1978@users.noreply.github.com>
2023-09-02 23:12:24 +0300

    commit_strukture_hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { Routes, Route, NavLink } from "react-router-dom";
+import Home from "../pages/Home";
+import Movies from "../pages/Movies";
+import NotFound from "../pages/NotFound";
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div>
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/movies">Movies</NavLink>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/movies/:movieId" />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </div>;
 };
