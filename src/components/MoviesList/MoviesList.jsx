5c17d2324382e5bb6bdafaab9ecc50fb048ae1de
Pavlo Yeromenko <108014692+Stranger1978@users.noreply.github.com>
2023-09-18 15:37:45 +0300

    Update MoviesList.jsx

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,15 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const MoviesList = ({ movies }) => { 
-    
-
+    const location = useLocation();
     return (
         <ul>
                 {movies.map(({ id, title }) => {
                     return (
                           <li key={id}> 
-                            <Link to={`/movies/${id}`}>{title}</Link>
+                            <Link to={`/movies/${id}`} state={{from: location}}>{title}</Link>
                         </li>
                     )
                 })}
